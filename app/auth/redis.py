d4f95f12efb7297c369e2e89d6d4fbde368a474e
Matt Howarth <msh32@njit.edu>
2026-04-20 23:58:56 -0400

    Update requirements and Add style.css

--- app/auth/redis.py
+++ app/auth/redis.py
@@ -1,9 +1,7 @@
-# app/auth/redis.py
 import redis.asyncio as redis
 from app.core.config import get_settings
 
 settings = get_settings()
-
 
 async def get_redis():
     if not hasattr(get_redis, "redis"):
@@ -15,12 +13,10 @@ async def get_redis():
 
 
 async def add_to_blacklist(jti: str, exp: int):
-    """Add a token's JTI to the blacklist"""
     redis_client = await get_redis()
     await redis_client.set(f"blacklist:{jti}", "1", ex=exp)
 
 
 async def is_blacklisted(jti: str) -> bool:
-    """Check if a token's JTI is blacklisted"""
     redis_client = await get_redis()
     return bool(await redis_client.exists(f"blacklist:{jti}"))
